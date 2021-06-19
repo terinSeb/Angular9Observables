9cbc305c89045c72aff06c6af4b8c9436b297ef8
Terin Sebastin <terin01@gmail.com>
2021-06-19 19:30:19 +0530

    InitialCommit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,44 @@
+import { OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit,OnDestroy {
+  private firstObsSubscription: Subscription;
+  constructor() { }
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+    const customIntervalObservable = Observable.create(observer =>{
+      let count = 0
+setInterval(() => {
+  observer.next(count);
+  if(count === 2){
+    observer.complete();
+  }
+  if(count > 3){
+    observer.error(new Error('Count is greater that 3'));    
+  }
+  count++;
+},1000)
+    });
+    this.firstObsSubscription =customIntervalObservable.subscribe((count: any) => {
+      console.log(count);
+    },error =>{
+alert(error.message);
+    },() =>
+    {
+      alert('Task Completed');
+    });
+  }
+
+}
